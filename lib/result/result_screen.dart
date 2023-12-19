3a47472ccd22200d063246ba79d93fbd7b6235f5
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-19 20:03:52 +0900

    056. 결과 화면에 if 문처리

--- lib/result/result_screen.dart
+++ lib/result/result_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ResultScreen extends StatefulWidget {
+class ResultScreen extends StatelessWidget {
   final double height;
   final double weight;
 
@@ -10,35 +10,56 @@ class ResultScreen extends StatefulWidget {
     required this.weight,
   }) : super(key: key);
 
-  @override
-  State<ResultScreen> createState() => _ResultScreenState();
-}
+  //메서드 result bmi 결과값
+  String _calBmi(double bmi) {
+    String result = '저체중';
+    if (bmi > 35) {
+      result = '고도비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과체중';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+    return result;
+  }
 
-class _ResultScreenState extends State<ResultScreen> {
+  // bmi 결과에 따라서 아이콘 변경 되는 로직
+  Widget _buildIcon(double bmi) {
+    Icon icon = Icon(Icons.sentiment_dissatisfied,color: Colors.red,);
+    if (bmi >= 23) {
+      icon = const Icon(Icons.sentiment_very_dissatisfied,color: Colors.yellow,);
+    } else if (bmi >= 18.5) {
+      icon = const Icon(Icons. sentiment_satisfied,color: Colors.green,);
+    }
+    return icon;
+  }
+
   //const 위젯 안에 변수가 있으면 const 를 붙이면 안되고 변수가없으면 붙인다.
   @override
   Widget build(BuildContext context) {
+    final double bmi = weight / ((height / 100) * (height / 100));
     String result = '정상';
+
     return Scaffold(
       appBar: AppBar(
         title: Text('결과'),
       ),
-      body:  Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              result,
+              _calBmi(bmi),
               style: const TextStyle(
                 fontSize: 36,
                 fontWeight: FontWeight.bold,
               ),
             ),
-           const Icon(
-              Icons.sentiment_satisfied,
-              color: Colors.green,
-              size: 100,
-            ),
+            _buildIcon(bmi),
           ],
         ),
       ),
