1a0ef95429329d41ed77d1b91f84158b5aeedf11
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-19 19:42:13 +0900

    055. const는 어디에 사용해야되나?

--- lib/result/result_screen.dart
+++ lib/result/result_screen.dart
@@ -15,24 +15,26 @@ class ResultScreen extends StatefulWidget {
 }
 
 class _ResultScreenState extends State<ResultScreen> {
+  //const 위젯 안에 변수가 있으면 const 를 붙이면 안되고 변수가없으면 붙인다.
   @override
   Widget build(BuildContext context) {
+    String result = '정상';
     return Scaffold(
       appBar: AppBar(
         title: Text('결과'),
       ),
-      body: const Center(
+      body:  Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '정상',
-              style: TextStyle(
+              result,
+              style: const TextStyle(
                 fontSize: 36,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Icon(
+           const Icon(
               Icons.sentiment_satisfied,
               color: Colors.green,
               size: 100,
